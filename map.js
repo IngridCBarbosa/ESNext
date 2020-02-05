d629cebf1b340045bbcafb490a28cdce0aafda96
INGRID <ingridcarolinelima@hotmail.com>
2020-02-05 10:48:31 -0300

    using Map

---
+++ map.js
@@ -0,0 +1,24 @@
+const tecnologias  = new Map()
+
+tecnologias.set('react', {framework: false})
+tecnologias.set('angular',{framework: false})
+
+console.log(tecnologias.react)
+console.log(tecnologias.get('react').framework )
+
+const chavesVariadas = new Map([
+    [function() {}, 'função'],
+    [{},'objeto'],
+    [123, 'numero'],
+
+])
+
+chavesVariadas.forEach((vl,ch) => {
+    console.log(ch,vl)
+})
+
+console.log(chavesVariadas.has(123))
+chavesVariadas.delete(123)
+console.log(chavesVariadas.has(123))
+console.log(chavesVariadas.size)
+console.log(chavesVariadas)
